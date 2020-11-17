15c3a60ccf93e9e7f124e52ff1480142ca73ccce
EC2 Default User <ec2-user@ip-172-31-6-175.ec2.internal>
2020-11-17 11:34:34 +0000

    finish

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,59 @@
+class BooksController < ApplicationController
+  before_action :ensure_correct_user, only: %i[edit update destroy]
+
+  def show
+    @books = Book.find(params[:id])
+    @user = @books.user
+    @book = Book.new
+  end
+
+  def index
+    @books = Book.all
+    @user = current_user
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    if @book.save
+      redirect_to book_path(@book), notice: "You have created book successfully."
+    else
+      @user = current_user
+      @books = Book.all
+      render 'index'
+    end
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book), notice: "You have updated book successfully."
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    if @book.destroy
+    redirect_to books_path
+    else render :show
+    end
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
+  def ensure_correct_user
+   user = Book.find(params[:id]).user
+   redirect_to books_path unless user == current_user
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  root 'homes#top'
+  get 'home/about' => 'homes#about', as: 'about'
+  devise_for :users
+
+  resources :users, only: [:show,:index,:edit,:update]
+  resources :books
+end
