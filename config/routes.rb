ed014985568f99fadfc960a1a117e071545390ba
EC2 Default User <ec2-user@ip-172-31-6-175.ec2.internal>
2020-11-21 12:52:35 +0000

    finish

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  get 'search/search'
   devise_for :users
   root 'homes#top'
   get 'home/about' => 'homes#about', as: 'about'
+  get '/search' => 'search#search'
   resources :users, only: [:show,:index,:edit,:update] do
     resource :relationships, only: [:create, :destroy]
      member do
