1fdf27f66955ac73e700464e429f7e37255e5c6d
EC2 Default User <ec2-user@ip-172-31-6-175.ec2.internal>
2020-11-25 03:33:34 +0000

    finish

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,6 +12,14 @@ class ApplicationController < ActionController::Base
   end
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:email])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [
+    :email,
+    :name,
+    :postcode,
+    :prefecture_name,
+    :address_city,
+    :address_street,
+    :address_building
+    ])
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -22,4 +22,15 @@ class User < ApplicationRecord
   def following?(user)
     following_user.include?(user)
   end
+  
+  include  JpPrefecture
+  jp_prefecture :prefecture_code
+  
+  def prefecture_name
+    JpPrefecture::Prefecture.find(code: prefecture_code).try(:name)
+  end
+  
+  def prefecture_name=(prefecture_name)
+    self.prefecture_code = JpPrefecture::Prefecture.find(name: prefecture_name).code
+  end
 end
